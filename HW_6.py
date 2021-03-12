bc27b7e9db1d0ebe18091d150b6bd237273d4769
Olesia Boichuk <oleska.boychuk@gmail.com>
2021-03-12 19:34:02 +0200

    Added HW_6.py

---
+++ HW_6.py
@@ -0,0 +1,307 @@
+import dataclasses
+from abc import ABC, abstractmethod
+
+VEGETABLES = ["Red_tomato", "Yellow", "Cherry", "Perfect"]
+FRUITS = ["Golden", "Yellow", "Antonivka"]
+
+states = {'nothing': 0, 'flowering': 1, 'green': 2, 'red': 3, 'rotten': 4}
+
+
+class GardenMetaClass(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Garden(metaclass=GardenMetaClass):
+    def __init__(self, vegetables, fruits, pests, gardener):
+        self.vegetables = vegetables
+        self.fruits = fruits
+        self.pests = pests
+        self.gardener = gardener
+
+    def show_the_garden(self):
+        print(f'The garden has such vegetables: {self.vegetables}')
+        print(f'Also garden has such fruits: {self.fruits}')
+        print(f'And such pests: {self.pests}')
+        print(f'The maintainer of the garden is {self.gardener}')
+
+
+@dataclasses.dataclass()
+class PlantsStates:
+    nothing: int
+    flowering: int
+    green: int
+    red: int
+    rotten: int
+
+
+class Vegetables(ABC):
+    def __init__(self, states, vegetable_type, name):
+        self.states = states
+        self.vegetable_type = vegetable_type
+        self.name = name
+
+    @property
+    def vegetable_type(self):
+        return self._vegetable_type
+
+    @vegetable_type.setter
+    def vegetable_type(self, vegetable_type):
+        if vegetable_type in VEGETABLES:
+            self._vegetable_type = vegetable_type
+            print('all ok')
+        else:
+            raise Exception(f'There is no such a vegetable in the list. Your vegetable: {vegetable_type}')
+
+    @abstractmethod
+    def grow(self):
+        raise NotImplementedError('Your method is not implemented.')
+
+    @abstractmethod
+    def is_ripe(self):
+        raise NotImplementedError('Your method is not implemented.')
+
+
+class Fruit(ABC):
+    def __init__(self, states, fruits_type, name):
+        self.states = states
+        self.fruits_type = fruits_type
+        self.name = name
+
+    @property
+    def fruits_type(self):
+        return self._fruits_type
+
+    @fruits_type.setter
+    def fruits_type(self, fruits_type):
+        if fruits_type in FRUITS:
+            self._fruits_type = fruits_type
+            print('all ok')
+        else:
+            raise Exception(f'There is no such a fruit in the list. '
+                            f'Your fruit {fruits_type} and list {FRUITS}')
+
+    @abstractmethod
+    def grow(self):
+        raise NotImplementedError('The method is missing.')
+
+    @abstractmethod
+    def is_ripe(self):
+        raise NotImplementedError('The method is missing.')
+
+
+class Gardener(ABC):
+    def __init__(self, name, plants):
+        self.name = name
+        self.plants = plants
+
+    @abstractmethod
+    def harvest(self):
+        raise NotImplementedError('The method is missing.')
+
+    @abstractmethod
+    def poison_pests(self):
+        raise NotImplementedError('The method is missing.')
+
+    @abstractmethod
+    def handling(self):
+        raise NotImplementedError('The method is missing.')
+
+    @abstractmethod
+    def check_states(self):
+        raise NotImplementedError('The method is missing.')
+
+
+class Pests(ABC):
+    def __init__(self, pests_type, quantity):
+        self.pests_type = pests_type
+        self.quantity = quantity
+
+    @abstractmethod
+    def eat(self):
+        raise NotImplementedError('Your method is not implemented')
+
+
+class Tomato(Vegetables):
+    def __init__(self, index, vegetable_type, states, name):
+        super(Tomato, self).__init__(states, vegetable_type, name)
+        self.index = index
+        self.vegetable_type = vegetable_type
+        self.state = 0
+
+    def grow(self):
+        self._change_state()
+
+    def is_ripe(self):
+        if self.state == 3:
+            return True
+        return False
+
+    def _change_state(self):
+        if self.state < 3:
+            self.state += 1
+        self.print_state()
+
+    def print_state(self):
+        if self.state == 0:
+            print(f'{self.vegetable_type} is only planted.')
+        elif self.state == 1:
+            print(f'{self.vegetable_type} is flowering.')
+        elif self.state == 2:
+            print(f'{self.vegetable_type} is not ready. Wait a little bit more.')
+        elif self.state == 3:
+            print(f"{self.vegetable_type} is ripe. It's time to harvest.")
+        else:
+            print(f"It's too late. {self.vegetable_type} is rotten.")
+
+
+class TomatoBush:
+    def __init__(self, num):
+        self.tomatoes = [Tomato(index, 'Red_tomato', states, 'Cherry') for index in range(0, num - 1)]
+
+    def grow_all(self):
+        for tomato in self.tomatoes:
+            tomato.grow()
+
+    def all_are_ripe(self):
+        return all([tomato.is_ripe() for tomato in self.tomatoes])
+
+    def provide_harvest(self):
+        self.tomatoes = []
+
+    def __call__(self):
+        return self.tomatoes
+
+
+class Apple(Fruit):
+    def __init__(self, index, fruits_type, states, name):
+        super(Apple, self).__init__(states, fruits_type, name)
+        self.index = index
+        self.fruits_type = fruits_type
+        self.state = 0
+
+    def grow(self):
+        self._change_state()
+
+    def is_ripe(self):
+        if self.state == 3:
+            return True
+        return False
+
+    def _change_state(self):
+        if self.state < 3:
+            self.state += 1
+        self.print_state()
+
+    def print_state(self):
+        if self.state == 0:
+            print(f'{self.fruits_type} is only planted.')
+        elif self.state == 1:
+            print(f'{self.fruits_type} is flowering.')
+        elif self.state == 2:
+            print(f'{self.fruits_type} is not ready. Wait a little bit more.')
+        elif self.state == 3:
+            print(f"{self.fruits_type} is ripe. It's time to harvest.")
+        else:
+            print(f"It's too late. {self.fruits_type} is rotten.")
+
+
+class AppleTree:
+    def __init__(self, num):
+        self.apples = [Apple(index, 'Golden', states, 'King') for index in range(0, num - 1)]
+
+    def grow_all(self):
+        for apple in self.apples:
+            apple.grow()
+
+    def all_are_ripe(self):
+        return all([apple.is_ripe() for apple in self.apples])
+
+    def provide_harvest(self):
+        self.apples = []
+
+    def __call__(self):
+        return self.apples
+
+
+class StarGardener(Gardener):
+    def __init__(self, name, plants):
+        super(StarGardener, self).__init__(name, plants)
+        self.name = name
+        self.plants = plants
+
+    def harvest(self):
+        print('Gardener is harvesting...')
+        for plant in self.plants:
+            if plant.all_are_ripe():
+                plant.provide_harvest()
+                print('Harvesting is finished.')
+            else:
+                print('Too early! Your plants are not ripe.')
+
+    def handling(self):
+        print('Gardner is working...')
+        for plant in self.plants:
+            plant.grow_all()
+        print('Gardner has finished')
+
+    def poison_pests(self):
+        for pest in range(pests.quantity):
+            pest += 1
+            print('Number of poisoned pests:', pest)
+        print("All pests are dead")
+
+    def check_states(self):
+        for all_plants in self.plants:
+            for plant in all_plants():
+                if plant.state == 3:
+                    return True
+                else:
+                    return False
+
+
+class PestsAreTheWorst(Pests):
+    def __init__(self, pests_type, quantity):
+        super(PestsAreTheWorst, self).__init__(pests_type, quantity)
+        self.pests_type = pests_type
+        self.quantity = quantity
+
+    def eat(self):
+        for tomato in range(len(Garden().vegetables)):
+            if self.quantity > 0:
+                tomato += 1
+                print('Number of tomatoes eaten by pests:', tomato)
+            else:
+                print('No pests to eat vegetables')
+        for apple in range(len(Garden().fruits)):
+            if self.quantity > 0:
+                apple += 1
+                print('Number of apples eaten by pests:', apple)
+            else:
+                print('No pests to eat vegetables')
+        print("All fruits and vegetables are eaten by pests")
+
+
+if __name__ == '__main__':
+
+    # Creating list of instances for vegetables and fruits, pests and gardener
+    tomato_bush = TomatoBush(4)
+    apple_tree = AppleTree(3)
+    pests = PestsAreTheWorst('worms', 3)
+    tom = StarGardener('Tom', [tomato_bush, apple_tree])
+    # creating only one garden instance with vegetables and fruits
+    garden = Garden(["Red_tomato", "Yellow", "Cherry", "Perfect"], ["Golden", "Yellow", "Antonivka"], "Worms", "Tom")
+    garden.show_the_garden()
+    state = tom.check_states()
+    # if not state:
+    #     gardener.handling()
+    for i in range(3):
+        tom.handling()
+    pests.eat()
+    tom.harvest()
+    tom.poison_pests()
